c252a18e17051003a70771e1df7f11651cbbf3af
Dulara07 <rachithadulara@gmail.com>
2026-02-08 23:22:03 +0530

    Updated Azure account with correct credentials

--- client/src/MSALApp.tsx
+++ client/src/MSALApp.tsx
@@ -2,11 +2,11 @@ import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 import { AppRouter } from './AppRouter';
 
-// TODO: Replace with your Azure AD app credentials
+// Azure AD app credentials
 const msalConfig = {
   auth: {
-    clientId: 'YOUR_AZURE_AD_CLIENT_ID',
-    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID',
+    clientId: '7f6a0b9b-f362-4417-853e-d1b962e4018d',
+    authority: 'https://login.microsoftonline.com/534253fc-dfb6-462f-b5ca-cbe81939f5ee',
     redirectUri: window.location.origin,
   },
 };
